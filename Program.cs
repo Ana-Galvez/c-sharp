e3446ae246b3908215210138085552e055e1c259
Ana Galvez <anamariagalvez01@gmail.com>
2024-10-29 03:01:13 -0300

    feat: arrays and foreach

--- Program.cs
+++ Program.cs
@@ -6,6 +6,17 @@ char aLetter='a';
 double number2= 22.232;
 string[] names={"ana","tatin","john","henry"};
 
+Console.WriteLine(names[0]);
+names[1]="otroTatin";
+Console.WriteLine(names[1]);
+
+string[] cars=new string[3] {"Ford","Fiat","Renault"};
+
+foreach (string car in cars)
+{
+    Console.WriteLine(car);
+}
+
 Console.WriteLine("Ingresa el primer número"); // WriteLine siempre devuelve un string
 int number1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("El número ingresado es: "+ number1);
