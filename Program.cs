fb8e75ee3f2b96df26b2ce102a8cd740e1a6de62
Ana Galvez <anamariagalvez01@gmail.com>
2024-10-30 01:26:54 -0300

    feat: fizzbuzz

--- Program.cs
+++ Program.cs
@@ -1,33 +1,53 @@
 ﻿using System;
 
-bool isOld=true;
-string myName="Ana Galvez";
-char aLetter='a';
-double number2= 22.232;
-string[] names={"ana","tatin","john","henry"};
+// bool isOld=true;
+// string myName="Ana Galvez";
+// char aLetter='a';
+// double number2= 22.232;
+// string[] names={"ana","tatin","john","henry"};
 
-Console.WriteLine(names[0]);
-names[1]="otroTatin";
-Console.WriteLine(names[1]);
+// Console.WriteLine(names[0]);
+// names[1]="otroTatin";
+// Console.WriteLine(names[1]);
 
-string[] cars=new string[3] {"Ford","Fiat","Renault"};
+// string[] cars=new string[3] {"Ford","Fiat","Renault"};
 
-foreach (string car in cars)
+// foreach (string car in cars)
+// {
+//     Console.WriteLine(car);
+// }
+
+// Console.WriteLine("Ingresa el primer número"); // WriteLine siempre devuelve un string
+// int number1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("El número ingresado es: "+ number1);
+// Console.WriteLine($"El número {number1} + 3 es: {number1 + 3}"); // traspolación
+// Console.WriteLine("---------------");
+// Console.WriteLine("Hello World!");
+
+// if (number1 >= 18)
+// {
+//     Console.WriteLine("This person is old");
+// }else
+// {
+//     Console.WriteLine("This person isn't old");
+// }
+//FizzBuzz
+for (int i = 1; i < 100;i++)
 {
-    Console.WriteLine(car);
-}
-
-Console.WriteLine("Ingresa el primer número"); // WriteLine siempre devuelve un string
-int number1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("El número ingresado es: "+ number1);
-Console.WriteLine($"El número {number1} + 3 es: {number1 + 3}"); // traspolación
-Console.WriteLine("---------------");
-Console.WriteLine("Hello World!");
-
-if (number1 >= 18)
-{
-    Console.WriteLine("This person is old");
-}else
-{
-    Console.WriteLine("This person isn't old");
+    if (i%3==0 && i%5==0)
+    {
+        Console.WriteLine("fizz-buzz");
+    }
+    else if (i%3==0)
+    {
+        Console.WriteLine("fizz");
+    }
+    else if (i%5==0)
+    {
+        Console.WriteLine("buzz");
+    }
+    else
+    {
+        Console.WriteLine(i);
+    }
 }
